3fb863bcda8d60356ab5e25981585fb4051785bb
Craig Lalumiere <clalumiere@inertiapd.com>
2025-08-28 19:01:06 -0400

    cubemx: pull downs on PWM outputs

--- Core/Src/stm32g4xx_hal_msp.c
+++ Core/Src/stm32g4xx_hal_msp.c
@@ -728,14 +728,14 @@ void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim)
         */
         GPIO_InitStruct.Pin       = PWM_A_H_Pin;
         GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
-        GPIO_InitStruct.Pull      = GPIO_NOPULL;
+        GPIO_InitStruct.Pull      = GPIO_PULLDOWN;
         GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_LOW;
         GPIO_InitStruct.Alternate = GPIO_AF4_TIM8;
         HAL_GPIO_Init(PWM_A_H_GPIO_Port, &GPIO_InitStruct);
 
         GPIO_InitStruct.Pin       = PWM_A_L_Pin;
         GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
-        GPIO_InitStruct.Pull      = GPIO_NOPULL;
+        GPIO_InitStruct.Pull      = GPIO_PULLDOWN;
         GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_LOW;
         GPIO_InitStruct.Alternate = GPIO_AF5_TIM8;
         HAL_GPIO_Init(PWM_A_L_GPIO_Port, &GPIO_InitStruct);
@@ -757,14 +757,14 @@ void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim)
         */
         GPIO_InitStruct.Pin       = PWM_B_L_Pin;
         GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
-        GPIO_InitStruct.Pull      = GPIO_NOPULL;
+        GPIO_InitStruct.Pull      = GPIO_PULLDOWN;
         GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_LOW;
         GPIO_InitStruct.Alternate = GPIO_AF1_TIM15;
         HAL_GPIO_Init(PWM_B_L_GPIO_Port, &GPIO_InitStruct);
 
         GPIO_InitStruct.Pin       = PWM_B_H_Pin;
         GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
-        GPIO_InitStruct.Pull      = GPIO_NOPULL;
+        GPIO_InitStruct.Pull      = GPIO_PULLDOWN;
         GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_LOW;
         GPIO_InitStruct.Alternate = GPIO_AF2_TIM15;
         HAL_GPIO_Init(PWM_B_H_GPIO_Port, &GPIO_InitStruct);
